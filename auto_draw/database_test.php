72d5a4092d69659f5e4b17f09807391d307a7f6d
QBZ (desktop) <borsodi.zoltan@gmail.com>
2021-11-17 23:49:21 +0100

    dbadd

--- auto_draw/database_test.php
+++ auto_draw/database_test.php
@@ -15,22 +15,22 @@ $tdbpassword="WRb4dKaeuSQKm7Q";
 $dbconnect=new mysqli($tserver,$tdbuser,$tdbpassword,$tdbname);
 
 if ($dbconnect->connect_error) { die("Nem siker�lt a csatlakoz�s: ".$dbconnect->connect_error); }
-echo "Sikeres csatlakoz�s!";
+echo "Sikeres csatlakozás!<br>";
 
-$title="Elso teszt esemeny";
-$startdate=date("Y.m.d");
-$drawtime=date("Y.m.d.H.i.s");
-$descrption="Az elso tesztesemeny reszletes leirasa";
-$ispub=true;
-$autoticket=false;
+$title="Ennek sikerülnie kellene elvileg";
+$startdate=date('Y-m-d H:m:s');
+$drawtime=date('Y-m-d H:m:s');
+$description="Az első tesztesemeny reszletes leírasa";
+$ispub=(int)'0';
+$autoticket=(int)'0';
 $location="Szombathely";
 $hash="EVENTHASH_1";
 
-$toadd="INSERT INTO events (title, start_time, draw_time, description, is_public, auto_ticket, location, hash) VALUES ($title, $startdate, $drawdate, $description, $ispub, $autoticket, $location, $hash)";
-//$add="INSERT INTO Prizes (prize_name, prize_description, event, prize_value, winner_ticket_id) VALUES ('els� teszt nyerem�ny', 'Az els� tesztnyerem�ny r�szletes le�r�sa', )"
+//$toadd="INSERT INTO ticketcolors (color_name) VALUES ('kek')";
+$sql="INSERT INTO events (title, start_time, draw_time, description, is_public, auto_ticket, location, hash) VALUES ('$title', '$startdate', '$drawtime', '$description', '$ispub', '$autoticket', '$location', '$hash')";
 
-if ($dbconnect->query($toadd)===true) { echo "�j rekord felv�tele sikeres"; }
-else { echo "Hiba az adatok hozz�ad�s�ban: ". $toadd . "<br>" . $dbconnect->error; }
+if ($dbconnect->query($sql)===true) { echo "Új rekord felvétele sikeres"; }
+else { echo "Hiba az adatok hozzáadásában: ". $sql . "<br>" . $dbconnect->error; }
 
 
 
