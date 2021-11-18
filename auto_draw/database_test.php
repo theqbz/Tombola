3448af1215ecde511836668f1f9d0d5358e255c3
QBZ (desktop) <borsodi.zoltan@gmail.com>
2021-11-18 23:02:46 +0100

    error logger

--- auto_draw/database_test.php
+++ auto_draw/database_test.php
@@ -5,34 +5,42 @@
 	<title>Ticketto DB test</title>
 </head>
 <body>
+<?php
 
-    <?php
-require_once 'db_auth_data.php';
+    require_once 'errorlogger.php';
+    require_once 'db_auth_data.php';
 
-$dbconnect=new mysqli($tserver,$tdbuser,$tdbpassword,$tdbname);
+    addToLogger("Start Ticketto AutoDraw", INFO);
 
-if ($dbconnect->connect_error) { die("Nem siker�lt a csatlakoz�s: ".$dbconnect->connect_error); }
-echo "Sikeres csatlakozás!<br>";
+    $dbconnect=new mysqli($tserver,$tdbuser,$tdbpassword,$tdbname);
+    if ($dbconnect->connect_error) {
+        addToLogger("Nem sikerült csatlakozni az adatbázishoz:".$dbconnect->connect_error, ERROR);
+        writeLog();
+        exit();
+    }
+    addToLogger("Adatbázis: sikeres csatlakozás.", INFO);
 
-$title="Ennek sikerülnie kellene elvileg";
-$startdate=date('Y-m-d H:m:s');
-$drawtime=date('Y-m-d H:m:s');
-$description="Az első tesztesemeny reszletes leírasa";
-$ispub=(int)'0';
-$autoticket=(int)'0';
-$location="Szombathely";
-$hash="EVENTHASH_1";
+    $title="Ennek sikerülnie kellene elvileg";
+    $startdate=date('Y-m-d H:m:s');
+    $drawtime=date('Y-m-d H:m:s');
+    $description="Az első tesztesemeny reszletes leírasa";
+    $ispub=(int)'0';
+    $autoticket=(int)'0';
+    $location="Szombathely";
+    $hash="EVENTHASH_1";
 
-//$toadd="INSERT INTO ticketcolors (color_name) VALUES ('kek')";
-$sql="INSERT INTO events (title, start_time, draw_time, description, is_public, auto_ticket, location, hash) VALUES ('$title', '$startdate', '$drawtime', '$description', '$ispub', '$autoticket', '$location', '$hash')";
+    //$toadd="INSERT INTO ticketcolors (color_name) VALUES ('kek')";
+    $sql="INSERT INTO events (title, start_time, draw_time, description, is_public, auto_ticket, location, hash) VALUES ('$title', '$startdate', '$drawtime', '$description', '$ispub', '$autoticket', '$location', '$hash')";
 
-if ($dbconnect->query($sql)===true) { echo "Új rekord felvétele sikeres"; }
-else { echo "Hiba az adatok hozzáadásában: ". $sql . "<br>" . $dbconnect->error; }
+    if ($dbconnect->query($sql)===true) { addToLogger("Új rekord felvétele sikeres.", INFO); }
+    else { addToLogger("Hiba az adatok hozzáadásában: ". $sql . "\n" . $dbconnect->error, ERROR); }
 
+    $dbconnect->close();
 
+    addToLogger("Exit AutoDraw", INFO);
+    writeLog();
+    echo($logtext);
 
-$dbconnect->close();
-
-    ?>
+?>
 </body>
 </html>
---
+++ auto_draw/errorlogger.php
@@ -0,0 +1,25 @@
+<?php
+// Ticketto AutoDraw logger system
+
+define("ERROR",1);
+define("INFO",2);
+
+$logtext="\n";
+$separator=" - ";
+
+function addToLogger($msg, $type)
+{
+    global $logtext, $separator;
+    if ($type==1) { $separator=" -! "; }
+    else if ($type==2) { $separator=" - "; }
+    $logtext.=date('Y.m.d H:i:s P').$separator.$msg."\n";
+}
+
+function writeLog()
+{
+    global $logtext;
+    $success=file_put_contents('./tdrawlog.txt',$logtext,FILE_APPEND);
+}
+
+
+?>
