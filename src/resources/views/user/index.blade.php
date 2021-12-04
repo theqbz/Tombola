21b8956d42c8da743b2e679cb89c309dd6088b09
QBZ (desktop) <borsodi.zoltan@gmail.com>
2021-12-04 19:38:20 +0100

    user/index (profiloldal)

--- src/resources/views/user/index.blade.php
+++ src/resources/views/user/index.blade.php
@@ -1,106 +1,103 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row justify-content-center">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-                @include('user.left')
+<div class="container-lg">
+<!--aloldal fejléc-->
+    <div class="row align-items-top mb-2">
+        <div class="col-sm-8 text-start mb-3">
+            <h1><div class="display-4">{{__("Profile Information")}}</div></h1>
+        </div>
+        <div class="col-sm-4 text-sm-end pt-3">
+            <a class="btn btn-primary" href="{{route('profile.edit')}}">
+                {{ __('Edit') }}
+            </a>
+        </div>
+    </div>
+<!-- adatlap részletei-->
+    <div class="row">
+        <div class="col-sm-4">
+            @include('user.left')
+        </div>
+        <div class="col-sm-8">
+            <div class="card">
+                <div class="card-header">
+                    <div class="h2 lead">{{__("Profile")}}</div>
                 </div>
-                <div class="col-md-8">
-                    <div class="card">
-
-                        <div class="card-header">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <h2>{{__("Profile")}}</h2>
-                                </div>
-                                <div class="col-md-6 text-right">
-                                    <a class="btn btn-primary" href="{{route('profile.edit')}}">
-                                        {{ __('Edit') }}
-                                    </a>
-                                </div>
-                            </div>
+                <div class="card-body">
+                    @isset($error)
+                        <div class="alert alert-danger">
+                            {{ $error}}
                         </div>
-                        <div class="card-body">
-                            @isset($error)
-                                <div class="alert alert-danger">
-                                    {{ $error}}
-                                </div>
-                            @endif
-                            @if(session()->has('success'))
-                                <div class="alert alert-success">
-                                    {{ session()->get('success') }}
-                                </div>
-                            @endif
-                            {{Form::model($user, array('route' => array('profile.update', $user->id)))}}
-                            <div class="form-group row">
-                                {{Form::label('email', __('Email'), array('class' => 'col-md-4 col-form-label text-md-right'))}}
-                                <div class="col-md-6">
+                    @endif
+                    @if(session()->has('success'))
+                        <div class="alert alert-success">
+                            {{ session()->get('success') }}
+                        </div>
+                    @endif
+                    {{Form::model($user, array('route' => array('profile.update', $user->id)))}}
+                    <div class="form-group row">
+                        {{Form::label('email', __('Email'), array('class' => 'col-md-4 col-form-label text-md-right'))}}
+                        <div class="col-md-6">
 
-                                    {{Form::text('email', $user->email,array('class'=>'form-control','readonly'=>'true'))}}
-
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                {{Form::label('first_name', __('First Name'), array('class' => 'col-md-4 col-form-label text-md-right'))}}
-                                <div class="col-md-6">
-
-                                    {{Form::text('first_name', $user->first_name,array('class'=>"form-control",'readonly'=>'true'))}}
-
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                {{Form::label('last_name', __('Last Name'), array('class' => 'col-md-4 col-form-label text-md-right'))}}
-                                <div class="col-md-6">
-
-                                    {{Form::text('last_name', $user->last_name,array('class'=>"form-control",'readonly'=>'true'))}}
-
-                                </div>
-                            </div>
-
-
-                            <div class="form-group row">
-                                {{Form::label('address', __('Address'), array('class' => 'col-md-4 col-form-label text-md-right'))}}
-                                <div class="col-md-6">
-
-                                    {{Form::text('address',$user->address,array('class'=>"form-control",'readonly'=>'true'))}}
-
-                                </div>
-                                @error('address')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-
-                            <div class="form-group row">
-                                {{Form::label('date_of_birth', __('Date Of Birth'), array('class' => 'col-md-4 col-form-label text-md-right'))}}
-                                <div class="col-md-6">
-                                    @php
-                                    $dobOld = isset($user->date_of_birth)?date('Y. m.d',strtotime($user->date_of_birth)):"";
-
-                                    @endphp
-                                    {{Form::text('date_of_birth', $dobOld,array('class'=>"form-control",'readonly'=>'true'))}}
-
-                                </div>
-                                @error('date_of_birth')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                            {{ Form::close() }}
+                            {{Form::text('email', $user->email,array('class'=>'form-control','readonly'=>'true'))}}
 
                         </div>
                     </div>
 
+                    <div class="form-group row">
+                        {{Form::label('first_name', __('First Name'), array('class' => 'col-md-4 col-form-label text-md-right'))}}
+                        <div class="col-md-6">
+
+                            {{Form::text('first_name', $user->first_name,array('class'=>"form-control",'readonly'=>'true'))}}
+
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        {{Form::label('last_name', __('Last Name'), array('class' => 'col-md-4 col-form-label text-md-right'))}}
+                        <div class="col-md-6">
+
+                            {{Form::text('last_name', $user->last_name,array('class'=>"form-control",'readonly'=>'true'))}}
+
+                        </div>
+                    </div>
+
+
+                    <div class="form-group row">
+                        {{Form::label('address', __('Address'), array('class' => 'col-md-4 col-form-label text-md-right'))}}
+                        <div class="col-md-6">
+
+                            {{Form::text('address',$user->address,array('class'=>"form-control",'readonly'=>'true'))}}
+
+                        </div>
+                        @error('address')
+                        <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+
+                    <div class="form-group row">
+                        {{Form::label('date_of_birth', __('Date Of Birth'), array('class' => 'col-md-4 col-form-label text-md-right'))}}
+                        <div class="col-md-6">
+                            @php
+                            $dobOld = isset($user->date_of_birth)?date('Y. m.d',strtotime($user->date_of_birth)):"";
+
+                            @endphp
+                            {{Form::text('date_of_birth', $dobOld,array('class'=>"form-control",'readonly'=>'true'))}}
+
+                        </div>
+                        @error('date_of_birth')
+                        <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    {{ Form::close() }}
                 </div>
             </div>
-
         </div>
     </div>
+</div>
 
 @endsection
--- src/resources/views/user/left.blade.php
+++ src/resources/views/user/left.blade.php
@@ -1,15 +1,15 @@
 <div class="card">
-
     <div class="card-header">
-        <h2>{{__("Access data")}}</h2>
+        <div class="h2 lead">{{__("Access data")}}</div>
     </div>
-    <div class="card-body">
-        <h3>{{__('Access code')}}</h3>
-        <div class="badge badge-info p-3 pb-0 text-uppercase text-white" style="font-size: 2.2rem">
+    <div class="card-body text-center text-sm-start ">
+        <!--
+        <div class="h3 small fw-bold text-uppercase">{{__('QR code')}}</div>
+        -->
+        <img style="width: 150px" src="{{asset('qrcodes/users/'.$user->id.'.svg')}}" alt="">
+        <div class="h3 small fw-bold text-uppercase mt-3">{{__('Profile ID')}}</div>
+        <div class="ps-2 text-center text-sm-start text-dark bg-ticketto text-uppercase fs-4 fw-bold">
             {{$user->getAccessCode()}}
         </div>
-        <hr>
-        <h3>{{__('QR code')}}</h3>
-        <img style="width: 150px" src="{{asset('qrcodes/users/'.$user->id.'.svg')}}" alt="">
     </div>
 </div>
