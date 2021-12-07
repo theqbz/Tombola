3725a53725dc9e41b09be9bf58935617e86d199d
QBZ (desktop) <borsodi.zoltan@gmail.com>
2021-12-07 22:57:04 +0100

    TempUserDashboard

--- src/resources/views/layouts/navbar/navbar_registered.blade.php
+++ src/resources/views/layouts/navbar/navbar_registered.blade.php
@@ -1,54 +1,27 @@
 <li class="nav-item">
-
     <a class="nav-link" href="{{route('mytickets')}}">Szelvényeim</a>
-
 </li>
-
 <li class="nav-item">
-
     <a class="nav-link" href="{{route('myprizes')}}">Nyereményeim</a>
-
 </li>
-
 <li class="nav-item">
-
     <a class="nav-link" href="{{route('connect')}}">Csatlakozás</a>
-
 </li>
-
 <li class="nav-item">
-
     <a class="nav-link" href="{{route('event.myevents')}}">Játékaim</a>
-
 </li>
-
 <li class="nav-item">
-
     <a class="nav-link" href="{{route('event.create')}}">Játék létrehozása</a>
-
 </li>
-
 <li class="nav-item">
-
     <a class="nav-link" href="{{route('event.index')}}">Publikus játékok</a>
-
 </li>
-
 <li class="nav-item">
-
     <a class="nav-link" href="{{route('profile.index')}}">Profil/beállítások</a>
-
 </li>
-
 <li class="nav-item d-md-none">
-
     <a class="nav-link" href="{{route('logout')}}">Kijelentkezés</a>
-
 </li>
-
 <li class="nav-item ms-2 d-none d-md-inline">
-
     <a class="btn btn-secondary" href="{{route('logout')}}">Kijelentkezés</a>
-
 </li>
-
