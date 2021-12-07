cd9cd06f402e3e5b01ae86abc01dae53c91d228a
QBZ (desktop) <borsodi.zoltan@gmail.com>
2021-12-07 16:29:33 +0100

    rend lett

--- src/resources/views/layouts/footer.blade.php
+++ src/resources/views/layouts/footer.blade.php
@@ -1,6 +1,10 @@
-<div class="container">
-    <span class="fw-bold text-ticketto">Ticketto</span>
-    <p class="text-light">Tombolajáték szervező és lebonyolító platform<br>
-        Készítik: Borsodi Zoltán, Eszenyi Ákos Zsolt, Tenk Norbert<br>
-        Projektmunka 1. (GKNB_INTM004) mérnökinformatika BSC, Széchenyi István Egyetem</p>
-</div>
+<div class="container">
+    <span class="fw-bold text-ticketto">Ticketto</span>
+    <p class="text-light">Tombolajáték szervező és lebonyolító platform<br>
+        Készítik: Borsodi Zoltán, Eszenyi Ákos Zsolt, Tenk Norbert<br>
+        Projektmunka 1. (GKNB_INTM004) mérnökinformatika BSC, Széchenyi István Egyetem</p>
+    <a class="text-light small text-decoration-none" href="https://github.com/theqbz/Tombola.git" target="_blanc">>> Ticketto on Github</a>
+</div>
+
+
+
